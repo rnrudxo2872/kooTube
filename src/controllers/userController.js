1aeb9e3d5c520abba4f175281225e1ad18e6d15a
unknown <rnrudxo2872@naver.com>
2021-06-05 18:45:11 +0900

    로그인 구현

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt"
 
 export const userDelete = (req,res) => {
     res.render("");
@@ -34,16 +35,39 @@ export const postJoin = async(req,res) =>{
     if(exists){
         return res.status(400).render("join",{pageTitle:"Join", errorMessage:"이미 있는 아이디거나 이메일 입니다."})
     }
-    await User.create({
-        email,
-        username,
-        password,
-        name,
-        location
-    });
-    res.redirect("/login")
+    try{
+        await User.create({
+            email,
+            username,
+            password,
+            name,
+            location
+        });
+        res.redirect("/login")
+    }catch(err){
+        return res.status(400).render("join",{pageTitle:"Join", errorMessage:err._message})
+    }
+
 ;}
 
-export const Login = (req,res) =>{
-    res.send("login")
+export const getLogin = (req,res) =>{
+    res.render("login", {pageTitle:"Login"})
+}
+
+export const postLogin = async(req,res) =>{
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+    if(!user){
+        return res.status(400).render("login",{pageTitle:"Login", errorMessage:"아이디가 존재하지 않습니다!"});
+    }
+    console.log(user);
+    console.log(password ,user.password);
+    const ok = await bcrypt.compare(password ,user.password);
+
+    if(!ok){
+        return res.status(400).render("login",{pageTitle:"Login", errorMessage:"비밀번호가 틀립니다!"});
+    }
+
+    console.log("User Login : ", user.email);
+    res.redirect("/");
 }
