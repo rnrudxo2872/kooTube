2058ea5feb9f6946ac355f7cb24697008fc68bc4
unknown <rnrudxo2872@naver.com>
2021-06-15 21:50:48 +0900

    setTimeout을 이용한 플레이어 뒤늦게 숨기기 -> 해당 setTimeout의 id를 받아 실행을 멈춤 (clearTimeout(id))

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -7,8 +7,11 @@ const totalTime = document.getElementById("totalTime");
 const timeline = document.getElementById("timeline");
 const fullScreenBtn = document.getElementById("fullScreen");
 const videoContainer = document.getElementById('videoContainer');
+const videoControls = document.getElementById("videoControls");
 
 const MEMO_VOLUME = "videoVolume";
+
+let controlsTimeout = null;
 
 video.volume = 0.5;
 localStorage.setItem(MEMO_VOLUME, video.volume);
@@ -74,10 +77,26 @@ const handleFullscreen = () =>{
     }
 }
 
+const handleMouseMove = () => {
+    if(controlsTimeout){
+        clearTimeout(controlsTimeout);
+        controlsTimeout = null;
+    }
+    videoControls.classList.add("showing");
+}
+
+const handleMouseLeave = () =>{
+    controlsTimeout = setTimeout(() => {
+        videoControls.classList.remove("showing");
+    }, 3000);
+}
+
 playBtn.addEventListener("click",handlePlayClick);
 muteBtn.addEventListener("click",handleMute);
 volumeRange.addEventListener("input",handleVolumeChange);
 video.addEventListener("loadedmetadata",handleLoadedMetadata);
 video.addEventListener("timeupdate",handleTimeUpdate);
 timeline.addEventListener("input",handleTimelineChange)
-fullScreenBtn.addEventListener("click",handleFullscreen)
+fullScreenBtn.addEventListener("click",handleFullscreen);
+video.addEventListener("mousemove", handleMouseMove);
+video.addEventListener("mouseleave", handleMouseLeave);
