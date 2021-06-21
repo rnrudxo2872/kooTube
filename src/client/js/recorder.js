f7090cd77dd4ce67da5aae7b55a6d49cf2941e90
unknown <rnrudxo2872@naver.com>
2021-06-22 01:00:52 +0900

    썸네일 넣을 준비

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,25 +1,45 @@
 import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 
-const startBtn = document.querySelector("#startBtn");
+const actionBtn = document.querySelector("#actionBtn");
 const video = document.querySelector("#preview");
 
 
 let stream;
 let recorder;
-let videoFile
+let videoFile;
+
+const files = {
+    input:"recording.webm",
+    output:"output.mp4",
+    thumb:"thumbnail.jpg"
+}
+
+const downloadFile = (fileURL, fileName) => {
+    const a = document.createElement("a");
+    a.href = fileURL;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+}
 
 const handleDownload = async() =>{
+    actionBtn.removeEventListener("click",handleDownload);
+
+    actionBtn.innerText = "파일 변환 중...";
+
+    actionBtn.disabled = true;
+
     const ffmpeg = createFFmpeg({log:true});
     await ffmpeg.load();
 
-    ffmpeg.FS("writeFile","recording.webm", await fetchFile(videoFile));
+    ffmpeg.FS("writeFile",files.input, await fetchFile(videoFile));
 
-    await ffmpeg.run("-i","recording.webm","-r","60","output.mp4");
+    await ffmpeg.run("-i",files.input,"-r","60",files.output);
 
-    await ffmpeg.run("-i","recording.webm","-ss","00:00:01","-frames:v","1","thumnail.jpg");
+    await ffmpeg.run("-i",files.input,"-ss","00:00:01","-frames:v","1",files.thumb);
 
-    const mp4File = ffmpeg.FS("readFile","output.mp4");
-    const thumbFile = ffmpeg.FS("readFile","thumnail.jpg");
+    const mp4File = ffmpeg.FS("readFile",files.output);
+    const thumbFile = ffmpeg.FS("readFile",files.thumb);
 
     console.log(mp4File);
     console.log(mp4File.buffer);
@@ -30,30 +50,25 @@ const handleDownload = async() =>{
     const mp4URL = URL.createObjectURL(mp4Blob);
     const thumURL = URL.createObjectURL(thumBlob);
 
-    const a = document.createElement("a");
-    a.href = mp4URL;
-    a.download = "MyRecording.mp4";
-    document.body.appendChild(a);
-    a.click();
+    downloadFile(mp4URL,"MyRecording.mp4");
+    downloadFile(thumURL,"MyThumbnail.jpg");
 
-    const thumA = document.createElement("a");
-    thumA.href = thumURL;
-    thumA.download = "MyThumbnail.jpg";
-    document.body.appendChild(thumA);
-    thumA.click();
+    actionBtn.disabled = false;
+    actionBtn.innerText = "녹화하기";
+    actionBtn.addEventListener("click",handleStart);
 }
 
 const handleStop = () => {
-    startBtn.innerText = "Download Recordeing";
-    startBtn.addEventListener("click",handleDownload);
-    startBtn.removeEventListener("click",handleStop);
+    actionBtn.innerText = "Download Recordeing";
+    actionBtn.addEventListener("click",handleDownload);
+    actionBtn.removeEventListener("click",handleStop);
     recorder.stop();
 }
 
 const handleStart = () =>{
-    startBtn.innerText = "Stop recording"
-    startBtn.removeEventListener("click",handleStart);
-    startBtn.addEventListener("click",handleStop);
+    actionBtn.innerText = "Stop recording"
+    actionBtn.removeEventListener("click",handleStart);
+    actionBtn.addEventListener("click",handleStop);
 
     recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
     recorder.ondataavailable  = (event) => {
@@ -80,4 +95,4 @@ const init = async() =>{
 }
 init();
 
-startBtn.addEventListener("click",handleStart);
+actionBtn.addEventListener("click",handleStart);
