a56829d970ed4bfb2e103db632d6077221484ee5
unknown <rnrudxo2872@naver.com>
2021-06-23 11:44:42 +0900

    댓글 form 로그인 안한 유저에서의 에러 fix

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,14 +1,17 @@
 const videoContainer = document.getElementById("videoContainer")
 const form = document.getElementById("commentForm");
-const textarea = form.querySelector("textarea");
-const btn = form.querySelector("button");
+
 
 const handleSubmit = (event) => {
     event.preventDefault();
     console.log(textarea.value);
     console.log(videoContainer.dataset);
+
+    const textarea = form.querySelector("textarea");
     const text = textarea.value;
     const video = videoContainer.dataset.id;
 }
 
-form.addEventListener("submit",handleSubmit)
+if(form){
+    form.addEventListener("submit",handleSubmit)
+}
